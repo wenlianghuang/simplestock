0cba53e1d56b9128c4378edd6cdb105aeb1820c3
wenlianghuang <wenliangmatt@gmail.com>
2024-03-10 21:10:05 +0800

    frontend/src

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import HomePage from './HomePage';
+import Chart from './Chart';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/chart">Chart</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/chart" element={<Chart />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
