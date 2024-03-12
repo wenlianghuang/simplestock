a51aea773546093ac595006bec024504fb075c16
wenlianghuang <wenliangmatt@gmail.com>
2024-03-12 22:58:23 +0800

    new

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomePage from './HomePage';
 import Chart from './components/Chart';
-
+import SaveContent from './components/SaveContent'
 const App = () => {
   
 
@@ -17,12 +17,16 @@ const App = () => {
             <li>
               <Link to="/chart">Chart</Link>
             </li>
+            <li>
+              <Link to="/savecontent">Save Content</Link>
+            </li>
           </ul>
         </nav>
 
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/chart" element={<Chart />} />
+          <Route path="/savecontent" element={<SaveContent/>} />
         </Routes>
       </div>
     </Router>
